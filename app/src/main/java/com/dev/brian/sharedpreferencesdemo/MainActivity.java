deb5ef44ac71ccfcdeb5fa00651446c74bb05325
brian <yuni0914@163.com>
2017-09-25 16:45:02 +0800

    提交代码

--- app/src/main/java/com/dev/brian/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/dev/brian/sharedpreferencesdemo/MainActivity.java
@@ -7,40 +7,42 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.dev.brian.sharedpreferencesdemo.util.SharedPrefsUtil;
+import com.dev.brian.sharedpreferencesdemo.utils.PreferencesUtils;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = "MainActivity";
     EditText edit_name;
     Button btn_login;
     String name;
-    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
+        // 初始化
         initView();
-        // 完成sp的初始化。
-        sp = getSharedPreferences("config", MODE_PRIVATE);
-        boolean isLogin = sp.getBoolean("save_name", true);//从sp中取出状态，判断是否已登录
-        if (isLogin) {
-            SharedPreferences.Editor editor = sp.edit();
-            editor.putBoolean("save_name", false);
-            editor.commit();
-            Log.d("test", "是第一次開啟");
-        } else {
-            Log.d("test", "不是第一次開啟");
-            Intent intent = new Intent(this, SuccessActivity.class);
-            name = sp.getString("pass", "");
-            intent.putExtra("name", name);
-            startActivity(intent);
-        }
+        boolean isLogin = PreferencesUtils.getBoolean(this, "save_name", true);
+        if (isLogin)
+            return;
+
+        name = PreferencesUtils.getString(this, "pass", "");
+        PreferencesUtils.putBoolean(this, "save_name", false);
+        // 获取文本框内容
+        Intent intent = new Intent(this, SuccessActivity.class);
+        // 传递文本框内容
+        intent.putExtra("name", name);
+        startActivity(intent);
+        finish();
     }
 
     private void initView() {
@@ -59,16 +61,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 name = edit_name.getText().toString();
                 // 验证
                 if (TextUtils.isEmpty(name)) {
-                    Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
-                    SharedPreferences.Editor editor = sp.edit();
-                    editor.putString("pass", name);
-                    editor.commit();
 
+                    Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+                    PreferencesUtils.putBoolean(this, "save_name", false);
+                    // 存储密码
+                    PreferencesUtils.putString(this, "pass", name);
+                    // 成功
                     Intent intent = new Intent(this, SuccessActivity.class);
                     intent.putExtra("name", name);
                     startActivity(intent);
+                    finish();
                 }
                 break;
         }
--- app/src/main/java/com/dev/brian/sharedpreferencesdemo/SuccessActivity.java
+++ app/src/main/java/com/dev/brian/sharedpreferencesdemo/SuccessActivity.java
@@ -5,8 +5,12 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.dev.brian.sharedpreferencesdemo.utils.PreferencesUtils;
 
 public class SuccessActivity extends AppCompatActivity implements View.OnClickListener {
     TextView txt_show;
@@ -16,10 +20,17 @@ public class SuccessActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_success);
         initView();
+        // 获取传递过来的数据
         String name = getIntent().getStringExtra("name");
         txt_show.setText(name);
+        // 权限判断
         if (name.equals("admin")) {
             btn_1.setVisibility(View.GONE);
         }
@@ -37,7 +48,11 @@ public class SuccessActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            // 注销
             case R.id.btn_cancel:
+                // 修改存储的账号
+                PreferencesUtils.putBoolean(this, "save_name", true);
+                PreferencesUtils.putString(this, "pass", null);
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 finish();
